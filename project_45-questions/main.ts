a5a75d878d7d9bee48ed6cffbadb9ca272de4688
MashoodBilal <44563181+MashoodBilal@users.noreply.github.com>
2024-01-17 18:33:43 +0500

    Add files via upload

---
+++ project_45-questions/main.ts
@@ -0,0 +1,103 @@
+/*
+// Question 2
+let name:string = "Mashood Bilal."
+let greet:string = "Hello"
+let message:string = "Its time for class."
+console.log(greet,name,message);
+*/
+/*
+// Question 3
+let fullName:string = "Mohammad Mashood Bilal"
+let loName = fullName.toLocaleLowerCase()
+console.log(loName);
+let upName = fullName.toLocaleUpperCase()
+console.log(upName);
+let tiName = fullName.split(" ")
+console.log(tiName);
+*/
+/*
+// Question 4
+let writer:string = "Mahmoud Darwaish"
+let message:string = "Exile is more than a geographical concept. You can be an exile in your homeland, in your own house, in a room."
+console.log(`${writer} once said, "${message}" `);
+*/
+/*
+// Question 5
+let famous_person = "Mahmoud Darwaish"
+let message2 = "History laughs at both the victim and the aggressor."
+console.log(`${famous_person} once said, "${message2}" `)
+*/
+/*
+// Question 6
+let tabname:string = "\t Mohammad Mashood Bilal. \n"
+console.log(tabname);
+let stripname:string = tabname.trim()
+console.log(stripname);
+*/
+/*
+// Question 7
+console.log(2+6)
+console.log(9-1)
+console.log(2*4)
+console.log(6/.75)
+*/
+/*
+// Question 8
+console.log(2+6)
+console.log(9-1)
+console.log(2*4)
+console.log(6/.75)
+*/
+/*
+// Question 9
+let num = 2
+console.log(`My Fav number is ${num}`)
+*/
+
+// Question 10
+// use ctrl +/ to comment.
+/*
+// Question 11
+let namearray:string[] = ["Mohammad", "Mashood", "Bilal"]
+console.log(namearray[1]);
+console.log(namearray[2]);
+console.log(namearray[0]);
+
+//  Question 12
+let greeting:string = "Hello ";
+console.log(greeting,namearray[0]);
+console.log(greeting,namearray[1]);
+console.log(greeting,namearray[2]);
+*/
+/*
+// Question 13
+let vehicle:string[] = ["sedan", "hatchback", "mini", "rickshaw", "motorbike", "bicycle", "onfoot"]
+let company:string[] = ["Honda", "Toyota", "Suzuki", "Aprili", "Unique"]
+console.log(`I would like to own a ${company[1]}'s ${vehicle[1]}`);
+*/
+
+// Question 14
+let guestlist:string[] = ["Mohammad", "Mashood", "Bilal"]
+let i = 0
+while( i < 3 ){
+console.log(`Dear ${guestlist[i]}, Its an request for having ypu here for dinner today at my place.`);
+i++
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
